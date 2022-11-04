58a19a9ecbc55f3ae20d2fc54346c38f3f9c412a
Ibra <lomeseth1@gmail.com>
2022-11-04 20:41:36 +0200

    init project

--- javascript.js
+++ javascript.js
@@ -8,18 +8,32 @@ function multiply(numb1,numb2){
     return numb1*numb2;
 }
 function divide(numb1,numb2){
-    if(numb2 == 0) return "verifyme";
+    if(numb2 == 0) return "Verify me";
     return numb1/numb2;
 }
 function operate(numb1,operator,numb2){
     if(operator =="+") return add(numb1,numb2);
     if(operator=="-") return subtract(numb1,numb2);
-    if(operator=="*") return multiplnumb2(numb1,numb2);
+    if(operator=="x") return multiply(numb1,numb2);
     if(operator=="/") return divide(numb1,numb2);
 }
 
 function display(e){
-    const numb = parseInt(this.className);
+    if(dis.innerText.length >30) return;
+    if(result != null && operator == null){
+        numb1 = null;
+        result = null;
+    }
+    let key;
+    if(window == this){
+         key = document.querySelector(`.digits button[data-key = "${e.keyCode}"]`);
+         if(key == null) return;
+    }
+    else{
+     key = this;
+}
+    const numb = parseInt(key.className);
+    let text = key.innerText;
     if(numb === 9999){
         dis.innerText = "0";
         cond = false;
@@ -30,7 +44,7 @@ function display(e){
     }
     if(numb == 110){
         if(point == false){
-            dis.innerText +=this.innerText;
+            dis.innerText +=text;
             cond = true;
             point = true;
         }
@@ -38,28 +52,36 @@ function display(e){
     }
     if(numb>=110 || numb<96) return;
     if(cond == false){
-    dis.innerText = this.innerText;
+    dis.innerText = text;
     if(numb != 96)
     cond = true;
     }
     else if(cond == true)
-    dis.innerText += this.innerText;
+    dis.innerText += text;
 }
 
 function operation(e){
-    console.log(this.innerText);
-    if(this.innerText == "="){
+    let text;
+    if(this == window){
+        let key = document.querySelector(`.operators button[data-key="${e.keyCode}"]`);
+        if(key == null) return;
+        text = key.innerText;
+    }
+    else{
+         text = this.innerText;
+    }
+    
+
+    if(text == "⏎"){
+        dis.innerText = dis.innerText.slice(0,dis.innerText.length-1); return;
+    }
+    if(text == "="){
         if(numb1 == null) return;
         numb2 = parseFloat(dis.innerText);
-        console.log("numb2 : " + numb2);
-        console.log("operator : " + operator);
-        switch (operator){
-            case "+" : result = add(numb1,numb2);console.log(1); break;
-            case "-" : result = subtract(numb1,numb2);console.log(2); break;
-            case "x" : result = multiply(numb1,numb2);console.log(3); break;
-            case "/" : result = divide(numb1,numb2);console.log(4); break;
-        }
-        console.log(result);
+        result = operate(numb1,operator,numb2);
+        if(typeof(result) == "number"){
+        result = Math.round(result*(10e15))/10e15;
+    }
         dis.innerText = result;
         numb1 = result;
         numb2 = null;
@@ -68,14 +90,13 @@ function operation(e){
         point = false;
     }
     else{
-     if(numb1 == null){
-    numb1 = parseFloat(dis.innerText);
-    console.log(numb1);
-    }
-    cond = false;
-    point = false;
-    operator = this.innerText;
-    dis.innerText="0";
+        if(numb1 == null){
+            numb1 = parseFloat(dis.innerText);
+        }
+        cond = false;
+        point = false;
+        operator = text;
+        dis.innerText="0";
 }
 }
 let result;
@@ -89,3 +110,6 @@ const digits = document.querySelectorAll(".digits button");
 digits.forEach((button)=> button.addEventListener("mousedown",display))
 const operators = document.querySelectorAll(".operators button" );
 operators.forEach(operator => operator.addEventListener("mousedown",operation));
+window.addEventListener("keydown",display);
+window.addEventListener("keydown",operation);
+
